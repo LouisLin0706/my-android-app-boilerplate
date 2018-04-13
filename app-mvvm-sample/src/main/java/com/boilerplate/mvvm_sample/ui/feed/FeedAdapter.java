f1ffc09618ab1c8992b443b8c505f89f7453c38e
林則儒 <stlin813@gmail.com>
2018-04-13 15:16:58 +0800

    set up lib-litho library & camera 2 sample project started

--- app-mvvm-sample/src/main/java/com/boilerplate/mvvm_sample/ui/feed/FeedAdapter.java
+++ app-mvvm-sample/src/main/java/com/boilerplate/mvvm_sample/ui/feed/FeedAdapter.java
@@ -1,8 +1,28 @@
 package com.boilerplate.mvvm_sample.ui.feed;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
 /**
  * Created by Louis on 2018/4/12.
  */
 
-public class FeedAdapter {
+public class FeedAdapter extends RecyclerView.Adapter {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Itemvid
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
---
+++ app-mvvm-sample/src/main/java/com/boilerplate/mvvm_sample/ui/feed/FeedItemViewModel.java
@@ -0,0 +1,8 @@
+package com.boilerplate.mvvm_sample.ui.feed;
+
+/**
+ * Created by Louis on 2018/4/13.
+ */
+
+public class FeedItemViewModel {
+}
